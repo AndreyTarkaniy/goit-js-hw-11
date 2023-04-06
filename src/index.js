a45bbcc59f1bddd10eef7e5376fd2cfc9f99dac8
AndreyTarkaniy <endrubass@gmail.com>
2023-04-06 15:45:48 +0300

    add increment and reset page + rendermarkup

--- src/index.js
+++ src/index.js
@@ -15,53 +15,56 @@ const refs = {
 refs.form.addEventListener('submit', handleSearchFotoSubmit);
 refs.loadMoreBtn.addEventListener('click', handleLoadMoreFotoClick);
 
-let searhcQuery = '';
-
 function handleSearchFotoSubmit(e) {
   e.preventDefault();
 
-  searhcQuery = e.target.elements.searchQuery.value;
-
-  newsApiService.fetchArticles(searhcQuery);
+  clearFotosSearch();
+  newsApiService.query = e.target.elements.searchQuery.value;
+  newsApiService.resetPage();
+  newsApiService.fetchArticles().then(appendFotosMarkup);
 }
 
-// function renderList(galleryItems) {
-//   const elementFoto = galleryItems
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) =>
-//         `<div class="gallery__item ">
-//         <a class= "gallery__link" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-
-//              <div class="info">
-//               <p class="info-item">
-//                 <b>Likes ${likes}</b>
-//               </p>
-//               <p class="info-item">
-//                 <b>Views ${views}</b>
-//               </p>
-//               <p class="info-item">
-//                 <b>Comments ${comments}</b>
-//               </p>
-//               <p class="info-item">
-//                 <b>Downloads ${downloads}</b>
-//               </p>
-//             </div>
-//           </div>`
-//     )
-//     .join('');
-
-//   refs.fotoCard.insertAdjacentHTML('beforeend', elementFoto);
-// }
-
 function handleLoadMoreFotoClick() {
-  newsApiService.fetchArticles(searhcQuery);
+  newsApiService.fetchArticles().then(appendFotosMarkup);
+}
+
+function appendFotosMarkup(hits) {
+  const elementFoto = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="gallery__item ">
+        <a class= "gallery__link" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+
+             <div class="info">
+              <p class="info-item">
+                <b>Likes ${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>Views ${views}</b>
+              </p>
+              <p class="info-item">
+                <b>Comments ${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>Downloads ${downloads}</b>
+              </p>
+            </div>
+          </div>`
+    )
+    .join('');
+
+  refs.fotoCard.insertAdjacentHTML('beforeend', elementFoto);
+}
+
+function clearFotosSearch() {
+  refs.fotoCard.innerHTML = '';
 }
 // const gallery = new SimpleLightbox('.gallery a');
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,12 +1,41 @@
 export default class NewsApiService {
-  constructor() {}
+  #KEY = '34996310-6eb230e6525d45c07c1c5f00a&q';
 
-  fetchArticles(searhcQuery) {
-    fetch(
-      `https://pixabay.com/api/?key=34996310-6eb230e6525d45c07c1c5f00a&q=${searhcQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    return fetch(
+      `https://pixabay.com/api/?key=${this.#KEY}&q=${
+        this.searchQuery
+      }&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${
+        this.page
+      }`
     )
       .then(response => response.json())
-      .then(console.log);
+      .then(data => {
+        this.incrementPage();
+        return data.hits;
+      });
+
     //   .then(data => renderList(data.hits));
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
